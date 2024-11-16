c264aae5e5351e1899291afc1f8cbe87a65f2834
Imtyaz <imtyazit17017@gmail.com>
2024-11-16 20:31:03 +0600

    bootstrap cdn added

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Discuss Board Project</title>
+    <?php include('./client/commonFiles.php')?>
 </head>
 <body>
-
+<?php include('./client/header.php')?>
 </body>
 </html>
